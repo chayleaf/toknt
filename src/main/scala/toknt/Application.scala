58e27d86b154b0d2b13f77b4fb68c97b39c88453
Superbox2147 <132390694+Superbox2147@users.noreply.github.com>
2025-09-21 05:37:38 +0300

    Format

--- src/main/scala/toknt/Application.scala
+++ src/main/scala/toknt/Application.scala
@@ -50,11 +50,15 @@ object Application {
                 }
               )
             )
-          case "kt" => Some(new KotlinLexer(charStream).getAllTokens.stream.filter(x => 
-            x.getType match {
-              case KotlinLexer.NL => false
-              case _ => true
-            }))
+          case "kt" =>
+            Some(
+              new KotlinLexer(charStream).getAllTokens.stream.filter(x =>
+                x.getType match {
+                  case KotlinLexer.NL => false
+                  case _              => true
+                }
+              )
+            )
           case _ => None
         }
       )
